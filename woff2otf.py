7485e6661f3cf4d83919a390c4d36e57efd673ff
ali <89098679+basisvectors@users.noreply.github.com>
2022-01-09 23:21:55 +0530

    Add files via upload

---
+++ woff2otf.py
@@ -0,0 +1,47 @@
+# import brotli !
+
+from fontTools.ttLib import woff2
+import os
+import sys
+
+def convert(infilename, outfilename):
+    with open(infilename , mode='rb') as infile:
+        with open(outfilename, mode='wb') as outfile:
+            woff2.decompress(infile, outfile)
+
+    return 0
+
+def batchconvert(source_dir, target_dir):
+    for file in os.listdir(source_dir):
+        if file.endswith('.woff2') or file.endswith('.woff'):
+            convert(source_dir + '/' + file, target_dir + '/' + file.rsplit('.', 1)[0] + '.otf')
+    
+    return 0
+
+
+def main(argv):
+    if len(argv) == 1 or len(argv) > 3:
+        print('I convert *.woff and *.woff2 files to *.otf files.\n'
+              'Usage: python woff2otf.py filename.woff or filename.woff2 or directory for batch conversion [target file name or target directory name]\n'
+              'If the target name is omitted, it will be guessed.\n')
+        return
+
+    if argv[1].endswith('.woff') or argv[1].endswith('.woff2'):
+        if len(argv) == 3:
+            target_file_name = argv[2]
+        else:
+            target_file_name = argv[1].rsplit('.', 1)[0] + '.otf'
+        convert(argv[1], target_file_name)
+    else:
+        if len(argv) == 3:
+            target_dir_name = argv[2]
+        else:
+            target_dir_name = argv[1] + '_otf'
+        if not os.path.exists(target_dir_name):
+            os.makedirs(target_dir_name)
+        batchconvert(argv[1], target_dir_name)
+    
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
